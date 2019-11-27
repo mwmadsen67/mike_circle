a4edd8eeac56ed370afc3686e52b8361f2ca3630
Mike Madsen <mikewmadsen@gmail.com>
2019-11-26 21:25:38 -0800

    Add default route for localhost:3000 as per akhtams request

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,6 @@
+# Need this controller to hit our default view from localhost:3000
+
+class StaticPagesController < ApplicationController
+  def root
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Use this if you want to route somewhere at localhost:3000
   root to: 'static_pages#root'
 
   resources :users, only: [:new, :create, :show]
